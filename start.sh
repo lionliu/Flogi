9c6ea6dbd6afd7d684ddb5765cbe70a86f11c48a
Leao Liu <llm2@cin.ufpe.br>
2020-10-19 14:03:26 -0300

    flogi-36: changed index-pattern creation order

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ echo 'waiting for Kibana server'
 until $(curl --silent --fail localhost:5601); do
     sleep 10
 done
-curl --silent -o /dev/null -X POST localhost:5601/api/saved_objects/index-pattern/fluentd -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d ' { "attributes": { "title": "fluentd-*", "timeFieldName": "@timestamp" } }'
-
 curl --silent -o /dev/null -X POST localhost:5601/api/saved_objects/index-pattern/metricbeat -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d ' { "attributes": { "title": "metricbeat-*", "timeFieldName": "@timestamp" } }'
+
+curl --silent -o /dev/null -X POST localhost:5601/api/saved_objects/index-pattern/fluentd -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d ' { "attributes": { "title": "fluentd-*", "timeFieldName": "@timestamp" } }'
 echo 'Kibana is up. Access localhost:5601'
